8dc966d65210cce09bc83ff3826f3f3e771b927a
Narko <nekit.2280.3390@gmail.com>
2023-04-08 00:50:46 +0300

    Added transitions to second and third fragments

--- app/src/main/java/com/narkorazdorov/android_pract_4/FragmentFirst.java
+++ app/src/main/java/com/narkorazdorov/android_pract_4/FragmentFirst.java
@@ -16,6 +16,25 @@ public class FragmentFirst extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_first, container, false);
+        Button button1 = view.findViewById(R.id.button1);
+        button1.setOnClickListener(view1 -> {
+            FragmentManager fragmentManager = getParentFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            FragmentSecond secondFragment = new FragmentSecond();
+            fragmentTransaction.replace(R.id.fragment_container, secondFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        });
+
+        Button button2 = view.findViewById(R.id.button2);
+        button2.setOnClickListener(view12 -> {
+            FragmentManager fragmentManager = getParentFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            FragmentThird thirdFragment = new FragmentThird();
+            fragmentTransaction.replace(R.id.fragment_container, thirdFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        });
         return view;
     }
 }
